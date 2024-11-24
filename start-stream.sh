f7321af7266523bec38af7e19d5b2170b147f108
VectorStudios <sakshamghotwadekarsggames@gmail.com>
2024-11-24 22:48:53 +0530

    Update start-stream.sh

--- start-stream.sh
+++ start-stream.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to your video
-VIDEO_PATH="./video.mp4"  # Adjust path if necessary
+VIDEO_PATH="http://vectorstudios.rf.gd/wp-content/uploads/2024/11/VID-20241124-WA0005.mp4"  # Adjust path if necessary
 
 # YouTube RTMP URL and stream key (replace YOUR_STREAM_KEY)
 STREAM_URL="rtmp://a.rtmp.youtube.com/live2/0kue-suc9-ec09-szbg-1qrd"
