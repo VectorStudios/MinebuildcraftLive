5444ff77f5362eb5bfe6885bf434166633c6117c
VectorStudios <sakshamghotwadekarsggames@gmail.com>
2024-11-24 22:50:04 +0530

    Update start-stream.sh

--- start-stream.sh
+++ start-stream.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Path to your video
-VIDEO_PATH="http://vectorstudios.rf.gd/wp-content/uploads/2024/11/VID-20241124-WA0005.mp4"  # Adjust path if necessary
+# Path to the video in the repository root
+VIDEO_PATH="http://vectorstudios.rf.gd/wp-content/uploads/2024/11/VID-20241124-WA0005.mp4"
 
-# YouTube RTMP URL and stream key (replace YOUR_STREAM_KEY)
-STREAM_URL="rtmp://a.rtmp.youtube.com/live2/0kue-suc9-ec09-szbg-1qrd"
+# YouTube RTMP URL and stream key
+STREAM_URL="rtmp://a.rtmp.youtube.com/live2/0kue-suc9-ec09-szbg-1qrd"  # Replace YOUR_STREAM_KEY with your YouTube stream key
 
-# Run FFmpeg to stream the video
-ffmpeg -re -i $VIDEO_PATH -vcodec libx264 -acodec aac -f flv $STREAM_URL
+# Run FFmpeg to stream the video on loop
+ffmpeg -stream_loop -1 -re -i $VIDEO_PATH -vcodec libx264 -preset ultrafast -tune zerolatency -acodec aac -strict experimental -f flv $STREAM_URL
+
+
+
